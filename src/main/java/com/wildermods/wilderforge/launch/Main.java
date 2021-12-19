5cc78f1f82f170f3c6707150e5799cd4043db9aa
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-12-19 17:44:52 -0500

    print classloader hierarchy

--- src/main/java/com/wildermods/wilderforge/launch/Main.java
+++ src/main/java/com/wildermods/wilderforge/launch/Main.java
@@ -1,11 +1,24 @@
 package com.wildermods.wilderforge.launch;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 public final class Main {
 
 	public static int x = 0;
 	
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println(FabricLoader.getInstance().getClass().getClassLoader());
+		
+		System.out.println("The classloader hierarchy is as follows: ");
+		printClassLoaderHierarchy(FabricLoader.getInstance().getClass().getClassLoader());
+	}
+	
+	public static void printClassLoaderHierarchy(ClassLoader classloader) {
+		ClassLoader currentLoader = classloader;
+		while(currentLoader != null) {
+			System.out.println(currentLoader);
+			currentLoader = currentLoader.getParent();
+		}
 	}
 	
 }
